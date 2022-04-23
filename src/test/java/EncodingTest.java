8ef97c33265485c209c671148017724258b23fb3
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-23 20:44:01 +0300

    Write test before implementing encryption behaviour.

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EncodingTest {
+    @Test
+    public void encryptedCharacter_returnsCharAtPlusTwoPosition() {
+        Encoding encoding= new Encoding();
+        Character expected = 'D';
+        assertEquals(expected, encoding.encryptCharacter('B'));
+    }
 }
