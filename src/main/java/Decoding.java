4fef47c615cce99df9eea920f111184ffb66e4c7
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-23 21:26:17 +0300

    Implement character decrypting behaviour.

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,2 +1,18 @@
+import java.util.Arrays;
+
 public class Decoding {
+    Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z'};
+
+    public Character diciperCharacter(char c) {
+        int position = Arrays.asList(alphabet).indexOf(c);
+        if(position -2 >= alphabet.length){
+            position=Math.abs(alphabet.length -position+2);
+        }else{
+            position=  position-2;
+        }
+        return alphabet[position];
+    }
 }
