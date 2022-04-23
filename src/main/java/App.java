937e9e58f91c9637c645e4b8922f52032366992e
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-23 21:11:41 +0300

    Implement setter and getter in main java class to encrypt user input.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner myEncryption = new Scanner(System.in);
+        Encoding encoding=new Encoding();
+        System.out.println("Enter text to encrypt:");
+        String input = myEncryption.nextLine();
+        encoding.setInput(input);
+
+        System.out.println("Enter encryption key to use:");
+        int key = myEncryption.nextInt();
+        encoding.setMkey(key);
+        System.out.println( "Plain Text:" + " " +encoding.getInput());
+        System.out.println( "Encryption Key:" + " " +encoding.getMkey());
+        System.out.println("Encrypted Text:"
+                + " " +encoding.wordEncryption(input));
 
     }
 }
