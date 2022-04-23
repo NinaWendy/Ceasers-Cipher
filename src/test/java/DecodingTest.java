117c02bb14766251de3a080ca7fc22bd06106836
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-23 21:25:36 +0300

    Write test before implementing decrypting behaviour.

--- src/test/java/DecodingTest.java
+++ src/test/java/DecodingTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DecodingTest {
+    @Test
+    public void dicipheredCharacter_returnsPlainCharacterAtMinusTwo() {
+        Decoding decoding=new Decoding();
+        Character expected ='A';
+        assertEquals(expected, decoding.diciperCharacter('C'));
+    }
 }
