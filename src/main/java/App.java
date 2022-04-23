46c543f108232f65f96082dc6a33d8b476f773f3
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-23 21:54:14 +0300

    Merge encryption and decryption feature.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner myEncryption = new Scanner(System.in);
-        Encoding encoding=new Encoding();
-        System.out.println("Enter text to encrypt:");
+        Encoding encoding= new Encoding();
+        Decoding decoding= new Decoding();
+
+        System.out.println("Enter text");
         String input = myEncryption.nextLine();
         encoding.setInput(input);
 
-        System.out.println("Enter encryption key to use:");
+        System.out.println("Enter key");
         int key = myEncryption.nextInt();
         encoding.setMkey(key);
+        decoding.setShiftkey(key);
+
         System.out.println( "Plain Text:" + " " +encoding.getInput());
         System.out.println( "Encryption Key:" + " " +encoding.getMkey());
         System.out.println("Encrypted Text:"
                 + " " +encoding.wordEncryption(input));
+
+        String encodedText= encoding.wordEncryption(input);
+        decoding.setEncodedText(encodedText);
+        System.out.println("Decrypted Text:"
+                + " " +decoding.wordDecryption(encodedText));
 
     }
 }
