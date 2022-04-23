254b900c114af3c3d01d4ca782706c3af5b8adaf
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-23 20:44:49 +0300

    Implementing character encryption behaviour.

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,2 +1,18 @@
+import java.util.Arrays;
+
 public class Encoding {
+    Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z'};
+
+    public Character encryptCharacter(char character){
+        int position = Arrays.asList(alphabet).indexOf(character);
+        if(position +2 >= alphabet.length){
+            position=Math.abs(alphabet.length -position-2);
+        }else{
+            position=  position+2;
+        }
+        return alphabet[position];
+    }
 }
