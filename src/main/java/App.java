738bfa758a1fbde4b5832baaec1fbf276d4dccb0
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-23 21:44:41 +0300

    Add prompts for user input and key.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner myEncryption = new Scanner(System.in);
+        Decoding decoding=new Decoding();
 
+        System.out.println("Enter text to decrypt:");
+        String encodedText = myEncryption.nextLine();
+        decoding.setEncodedText(encodedText);
+
+        System.out.println("Enter key used to encrypt:");
+        int shiftKey = myEncryption.nextInt();
+        decoding.setShiftkey(shiftKey);
+
+        System.out.println( "Input:" + " " +decoding.getEncodedText());
+        System.out.println( "Encryption Key:" + " " +decoding.getShiftkey());
+        System.out.println("Encrypted text:"
+                + " " +decoding.wordDecryption(encodedText));
     }
 }
